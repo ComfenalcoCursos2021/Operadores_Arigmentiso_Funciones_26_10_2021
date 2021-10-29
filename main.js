79830dce64677c1b5dbbaedba6b873947a440493
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-10-28 19:11:49 -0500

    Segun parte de los operadores arigmeticos

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
-
-
-
-let myStyle = `font-family:sans-serif;
+let myStyle = `font-family:monospace;
                 color:#fff;
                 font-weight: 500;
                 border-radius: 10px;
                 background:#00A7AF;
                 padding: 5px;`;
-
-
-function suma(numero1, numero2, myStyle) {
+function Suma(numero1, numero2, myStyle) {
     let resultado = numero1 + numero2;
     console.group("Funcion SUMA");
         console.log(`%cEl numero1: N° ${numero1}`, myStyle);
-        console.log(`%cEl numero2: N° ${numero1}`, myStyle);
+        console.log(`%cEl numero2: N° ${numero2}`, myStyle);
         console.log(`%cResultado de la suma: E° ${resultado}`, myStyle);
     console.groupEnd();
 
 }
+function Elevar(numero1, numero2, myStyle) {
+    let resultado = numero1 ** numero2;
+    console.group("Funcion ELEVEAR");
+        console.log(`%cEl numero1: N° ${numero1}`, myStyle);
+        console.log(`%cEl numero2: N° ${numero2}`, myStyle);
+        console.log(`%cResultado al elevar el numero: E° ${resultado}`, myStyle);
+    console.groupEnd();
+}
+function Modulo(numero1, numero2, myStyle) {
+    let resultado = numero1 % numero2;
+    console.group("Funcion MODULO");
+        console.log(`%cEl numero1: N° ${numero1}`, myStyle);
+        console.log(`%cEl numero2: N° ${numero2}`, myStyle);
+        console.log(`%cEl sobrante de la division : E° ${resultado}`, myStyle);
+    console.groupEnd();
+}
 
-suma(98,24,myStyle);
+
+
+
+Suma(98,24,myStyle);
+Elevar(2,4,myStyle);
+Modulo(11,3,myStyle);
